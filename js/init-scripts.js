e18d8f396dac3305436ec75ac3f6769c45a30cd5
unknown <rafal-zerlak@ILT-BM23N4J.Huntingdon.ac.uk>
2015-12-07 11:39:31 +0000

    test work

---
+++ js/init-scripts.js
@@ -0,0 +1,16 @@
+
+(function($) {
+
+    new WOW().init();
+
+})(jQuery);
+
+//var wow = new WOW(
+//  {
+//    boxClass:     'wow',      // animated element css class (default is wow)
+//    animateClass: 'animated', // animation css class (default is animated)
+//    offset:       50,          // distance to the element when triggering the animation (default is 0) 
+//    mobile:       false   
+//  }
+//);
+//wow.init();
